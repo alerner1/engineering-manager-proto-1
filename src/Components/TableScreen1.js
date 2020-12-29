19a3a45e6118092f520694117630723b32c28d27
alerner1 <alyssaplerner@gmail.com>
2020-12-28 19:46:08 -0500

    redesigned percentage bar

--- src/Components/TableScreen1.js
+++ src/Components/TableScreen1.js
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import { v4 as uuidv4 } from 'uuid';
-import { VictoryChart, VictoryAxis, VictoryBar, VictoryLabel } from 'victory';
+import { VictoryChart, VictoryAxis, VictoryBar, VictoryLabel, VictoryStack } from 'victory';
 import React, {useEffect} from 'react';
 
 const TableScreen1 = ({ data, percentageMetric }) => {
@@ -26,7 +26,7 @@ const TableScreen1 = ({ data, percentageMetric }) => {
         <td>
           {row["System"]}
         </td>
-        <td>
+        <td className="with-chart">
           {createBar(row[percentageMetric])}
         </td>
         <td>
@@ -85,29 +85,44 @@ const TableScreen1 = ({ data, percentageMetric }) => {
   const createBar = (dataPoint) => {
     const formattedData = [{ x: 0, y: dataPoint }];
     
-    let color = "GoldenRod";
+    let color = "green";
     if (dataPoint > 33 && dataPoint < 67) {
-      color = "DarkOrange";
+      color = "#E0BA3E";
     } else if (dataPoint > 67) {
-      color = "DarkRed";
+      color = "red";
     }
 
     return (
-      <Row noGutters>
-        <Col xs={9}>
-          <VictoryChart maxDomain={{y: 100}} horizontal height={20} width={100} padding={5}>
-            <VictoryBar 
-              style={{ data: {fill: color, width: 25 }, labels: { fill: "transparent" }}}
-              data={formattedData} 
-              labels={({datum}) => (`${datum.y}`)} 
-            />
+      
+          <VictoryChart maxDomain={{y: 100}} horizontal height={15} width={100} padding={1}>
+            <VictoryStack
+              style={{ data: { width: 25 }, labels: { fill: "white", fontSize: 9 }}}
+            >
+
+              <VictoryBar 
+                cornerRadius={{ bottomLeft: 5, bottomRight: 5 }}
+                style={{ data: { fill: color, stroke: "black", strokeWidth: 1 }}}
+                data={formattedData} 
+                labels={dataPoint} 
+                labelComponent={
+                  <VictoryLabel x={dataPoint} dx={-dataPoint * 0.01} textAnchor="middle"  verticalAnchor="middle" />
+                }
+                />
+              <VictoryBar
+                cornerRadius={5}
+                style={{ data: { fill: "darkslategray", stroke: "black", strokeWidth: 1 } }}
+                data={[{ x: 0, y: 100 - dataPoint }]}
+              />
+            </VictoryStack>
             <VictoryAxis
               style={{
                 axis: { stroke: "transparent" },
-                ticks: { stroke: "transparent" },
+                ticks: { stroke: "black" },
                 tickLabels: { fill: "transparent" }
               }}
-            />
+              tickValues={[0]}
+              tickCount={1}
+              />
             <VictoryAxis
               dependentAxis
               style={{
@@ -115,14 +130,8 @@ const TableScreen1 = ({ data, percentageMetric }) => {
                 tick: { stroke: "transparent" },
                 tickLabels: { fill: "transparent" }
               }}
-            />
+              />
           </VictoryChart>
-        </Col>
-        <Col xs={3}>
-
-        {dataPoint}
-        </Col>
-      </Row>
     );
   }
 
