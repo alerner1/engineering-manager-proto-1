a7be677f2d4f6578e1dfb1f7323e05cba0da2fef
alerner1 <alyssaplerner@gmail.com>
2021-01-04 17:41:18 -0500

    added alert symbols

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Screen3Container from './Screen3/Containers/Screen3Container';
 import React, {useState, useEffect} from 'react';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons'
+import { faPlusCircle, faMinusCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faPlusCircle, faMinusCircle)
+library.add(faPlusCircle, faMinusCircle, faExclamationTriangle)
 
 const App = () => {
 
--- src/Screen3/Components/RiskAlerts.js
+++ src/Screen3/Components/RiskAlerts.js
@@ -1,5 +1,6 @@
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const RiskAlerts = () => {
   return(
@@ -11,18 +12,28 @@ const RiskAlerts = () => {
         <Card.Text>
           <ListGroup variant="flush">
             <ListGroup.Item className="font-small small-padding">
+            <FontAwesomeIcon style={{color: 'red'}} className="hover-hand" icon="exclamation-triangle" /> 
+            &nbsp;
               Design final iteration was rushed and close to deadline
             </ListGroup.Item>
             <ListGroup.Item className="font-small small-padding">
+              <FontAwesomeIcon style={{color: 'red'}} className="hover-hand" icon="exclamation-triangle" /> 
+              &nbsp;
               SME review was waived
             </ListGroup.Item>
             <ListGroup.Item className="font-small small-padding">
+              <FontAwesomeIcon style={{color: 'red'}} className="hover-hand" icon="exclamation-triangle" /> 
+              &nbsp;
               Review comments include "Hesitations", "need more discussion", "upcoming deadline"
             </ListGroup.Item>
             <ListGroup.Item className="font-small small-padding">
+              <FontAwesomeIcon style={{color: 'red'}} className="hover-hand" icon="exclamation-triangle" /> 
+              &nbsp;
               Design owner was junior and new to department
             </ListGroup.Item>
             <ListGroup.Item className="font-small small-padding">
+              <FontAwesomeIcon style={{color: 'red'}} className="hover-hand" icon="exclamation-triangle" /> 
+              &nbsp;
               No senior manager approved
             </ListGroup.Item>
           </ListGroup>
