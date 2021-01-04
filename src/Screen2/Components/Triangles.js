d9742fffe298e953392b819a0f16a82c45a804c4
alerner1 <alyssaplerner@gmail.com>
2021-01-03 22:26:05 -0500

    critical triangles are outlined in dark red

--- src/Screen2/Components/Triangles.js
+++ src/Screen2/Components/Triangles.js
@@ -18,15 +18,26 @@ const Triangles = ({row}) => {
     } else {
       return 'green';
     }
-  }
+  };
+
+  const getStrokeColor = () => {
+    const topColor = getColor(row["% Schedule delays"]);
+    const bottomColor = getColor(row["% Design Errors"]);
+
+    if (topColor === 'red' && bottomColor === 'red' || topColor === 'red' && bottomColor === '#E0BA3E' || topColor === '#E0BA3E' && bottomColor === 'red') {
+      return 'darkred';
+    } else {
+      return 'black';
+    }
+  };
 
   const triangleWidth = window.innerWidth/10
 
   return(
     <Stage ref={elementRef} width={width} height={40}>
       <Layer>
-        <Line closed points={[(width - triangleWidth)/2, 0, (width - triangleWidth)/2 + triangleWidth, 0, (width - triangleWidth)/2 + triangleWidth, 40]} fill={getColor(row["% Schedule delays"])} stroke='black' strokeWidth={1} />
-        <Line closed points={[(width - triangleWidth)/2, 0, (width - triangleWidth)/2, 40, (width - triangleWidth)/2 + triangleWidth, 40]} fill={getColor(row["% Design Errors"])} stroke='black' strokeWidth={1} />
+        <Line closed points={[(width - triangleWidth)/2 + 2, 0, (width - triangleWidth)/2 + triangleWidth, 0, (width - triangleWidth)/2 + triangleWidth, 40]} fill={getColor(row["% Schedule delays"])} stroke={getStrokeColor()} strokeWidth={2} />
+        <Line closed points={[(width - triangleWidth)/2, 0, (width - triangleWidth)/2, 40, (width - triangleWidth)/2 + triangleWidth, 40]} fill={getColor(row["% Design Errors"])} stroke={getStrokeColor()} strokeWidth={2} />
       </Layer>
     </Stage>
   );
