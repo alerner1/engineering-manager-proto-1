8d4b5d0c342947974128495947b7f36a284ed713
alerner1 <alyssaplerner@gmail.com>
2021-01-17 16:55:58 -0500

    added screen 4 link

--- src/Screen4/Containers/Screen4Container.js
+++ src/Screen4/Containers/Screen4Container.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const Screen4Container = () => {
+  // so here we have screen 4
+  // if we're given a division, just display all parts for entire division
+  // if subdivision, display for only that subdivision
+  // otherwise it's weird because default screen doesn't have the links to screen 4
+  // since subdivs aren't displayed by default
+
+  // [product line dropdown] [severity dropdown] [time frame (dropdown? not sure what this is exactly)]
+
+  // table
+  // part | risk alerts (#) | blockers (#? str?) | process deviations (#)
+
   return('hi')
 };
 
