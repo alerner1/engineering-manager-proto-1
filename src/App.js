4af601bc22ab2dcacaed19fd92c672e504127a26
alerner1 <alyssaplerner@gmail.com>
2021-02-10 16:05:38 -0500

    added new icons to sidebar

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import TrendsPageContainer from './TrendsPage/Containers/TrendsPageContainer'
 import React, { useState } from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPlusCircle, faMinusCircle, faExclamationTriangle, faCircle, faBars, faAtom, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
+import { faPlusCircle, faMinusCircle, faExclamationTriangle, faCircle, faBars, faAtom, faLayerGroup, faChartLine, faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
 import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent } from 'react-pro-sidebar';
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-library.add(faPlusCircle, faMinusCircle, faExclamationTriangle, faCircle, faBars, faAtom, faLayerGroup)
+library.add(faPlusCircle, faMinusCircle, faExclamationTriangle, faCircle, faBars, faAtom, faLayerGroup, faChartLine, faProjectDiagram)
 
 const App = () => {
 
@@ -34,7 +34,7 @@ const App = () => {
           </SidebarHeader>
           <SidebarContent>
           <Menu>
-            <SubMenu className="ml-3" title="Analytics" icon={<FontAwesomeIcon icon="atom" />}>
+            <SubMenu className="ml-3" title="Analytics" icon={<FontAwesomeIcon icon="chart-line" />}>
               <MenuItem>
                 Dashboards & KPIs
                 <Link to="/" />
@@ -50,7 +50,7 @@ const App = () => {
               <MenuItem>Alerts</MenuItem>
               <MenuItem>Workflows</MenuItem>
             </SubMenu>
-            <SubMenu className="ml-3" title="Customize" icon={<FontAwesomeIcon icon="atom" />}>
+            <SubMenu className="ml-3" title="Customize" icon={<FontAwesomeIcon icon="project-diagram" />}>
               <MenuItem>Application settings</MenuItem>
               <MenuItem>Users & accounts</MenuItem>
               <MenuItem>Analytics</MenuItem>
