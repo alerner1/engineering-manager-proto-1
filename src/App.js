17d6b5fe35b22523bd284c191515a118aacb4264
alerner1 <alyssaplerner@gmail.com>
2021-02-10 15:24:35 -0500

    fixed sidebar and adjusted styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import Screen2Container from './Screen2/Containers/Screen2Container';
 import Screen3Container from './Screen3/Containers/Screen3Container';
 import Screen4Container from './Screen4/Containers/Screen4Container';
 import TrendsPageContainer from './TrendsPage/Containers/TrendsPageContainer'
-import React, { useState, useEffect } from 'react';
-import { Route, Switch, useHistory, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPlusCircle, faMinusCircle, faExclamationTriangle, faCircle, faBars, faAtom, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent } from 'react-pro-sidebar';
 import Button from 'react-bootstrap/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -19,62 +17,46 @@ const App = () => {
 
   const [showSidebar, setShowSidebar] = useState(true);
 
-  let history = useHistory();
-
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar)
   }
 
-  // const handleNav = (event) => {
-  //   console.log(event.target)
-  //   switch(event.target.id) {
-  //     case "dashboard":
-  //       history.push('/');
-  //       break;
-  //     case "trends":
-  //       history.push('/trends')
-  //     default:
-  //       history.push('/');
-  //       break;
-  //   }
-  // }
-
   return (
-      <div className="d-flex">
-        <div >
-          <div className="sticky-top" style={{height: '100vh', backgroundColor: 'whitesmoke'}}>
+      <div className="d-flex" style={{height: '100%'}}>
+        <ProSidebar collapsed={!showSidebar}>
+          <SidebarHeader>
             <Button className="shadow-none button-transparent" onClick={() => toggleSidebar()}>
-              <FontAwesomeIcon style={{color: 'midnightblue'}} icon="bars" /> 
+              <FontAwesomeIcon icon="bars" /> 
             </Button>
-            <ProSidebar collapsed={!showSidebar}>
-              <Menu>
-                <SubMenu title="Analytics" icon={<FontAwesomeIcon style={{color: 'midnightblue'}} icon="atom" />}>
-                  <MenuItem>
-                    Dashboards & KPIs
-                    <Link to="/" />
-                  </MenuItem>
-                  <MenuItem>
-                    Trends & performance <br /> tracking
-                    <Link to="/trends" />
-                  </MenuItem>
-                  <MenuItem>Drivers of errors & <br /> process recommendations</MenuItem>
-                  <MenuItem>Predicted Risk</MenuItem>
-                </SubMenu>
-                <SubMenu title="Automation" icon={<FontAwesomeIcon style={{color: 'midnightblue'}} icon="atom" />}>
-                  <MenuItem>Alerts</MenuItem>
-                  <MenuItem>Workflows</MenuItem>
-                </SubMenu>
-                <SubMenu title="Customize" icon={<FontAwesomeIcon style={{color: 'midnightblue'}} icon="atom" />}>
-                  <MenuItem>Application settings</MenuItem>
-                  <MenuItem>Users & accounts</MenuItem>
-                  <MenuItem>Analytics</MenuItem>
-                  <MenuItem>Automation</MenuItem>
-                </SubMenu>
-              </Menu>
-            </ProSidebar>
-          </div>
-        </div>
-        <div>
+          </SidebarHeader>
+          <SidebarContent>
+          <Menu>
+            <SubMenu className="ml-3" title="Analytics" icon={<FontAwesomeIcon icon="atom" />}>
+              <MenuItem>
+                Dashboards & KPIs
+                <Link to="/" />
+              </MenuItem>
+              <MenuItem>
+                Trends & performance <br /> tracking
+                <Link to="/trends" />
+              </MenuItem>
+              <MenuItem>Drivers of errors & <br /> process recommendations</MenuItem>
+              <MenuItem>Predicted Risk</MenuItem>
+            </SubMenu>
+            <SubMenu className="ml-3" title="Automation" icon={<FontAwesomeIcon icon="atom" />}>
+              <MenuItem>Alerts</MenuItem>
+              <MenuItem>Workflows</MenuItem>
+            </SubMenu>
+            <SubMenu className="ml-3" title="Customize" icon={<FontAwesomeIcon icon="atom" />}>
+              <MenuItem>Application settings</MenuItem>
+              <MenuItem>Users & accounts</MenuItem>
+              <MenuItem>Analytics</MenuItem>
+              <MenuItem>Automation</MenuItem>
+            </SubMenu>
+          </Menu>
+          </SidebarContent>
+        </ProSidebar>
+        <div style={{width: '100%', overflow: 'scroll'}}>
           <Switch>
             <Route path="/subdivisions/:name" children={<Screen2Container />} />
             <Route path="/design_errors/:name" children={<Screen4Container />} />
